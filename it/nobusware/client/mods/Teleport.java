18ef7ffcb2ec5c792297e4337f0a19e89d34d0d7
ChristopherProject <cri.russ2000@gmail.com>
2020-11-29 19:21:05 +0100

    Fixed Teleport

--- it/nobusware/client/mods/Teleport.java
+++ it/nobusware/client/mods/Teleport.java
@@ -1,12 +1,24 @@
 package it.nobusware.client.mods;
 
+import java.awt.Color;
+
 import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.GL11;
 
 import QuarantineAPI.config.annotation.Handler;
+import it.nobusware.client.events.EventRenderer3D;
+import it.nobusware.client.events.EventRendererGUI2D;
 import it.nobusware.client.events.EventUpdate;
 import it.nobusware.client.manager.Module;
+import it.nobusware.client.utils.ColorUtilsArray;
+import it.nobusware.client.utils.RenderUtils;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.player.PlayerCapabilities;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.C03PacketPlayer;
+import net.minecraft.network.play.client.C13PacketPlayerAbilities;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
 
@@ -25,6 +37,16 @@ public class Teleport extends Module {
 		if (ray == null)
 			return;
 		if (Mouse.isButtonDown(1)) {
+			
+			PlayerCapabilities pc = new PlayerCapabilities();
+			pc.disableDamage = false;
+			pc.isFlying = false;
+			pc.allowFlying = false;
+			pc.isCreativeMode = false;
+			pc.setFlySpeed(0.0F);
+			pc.setPlayerWalkSpeed(0.0F);
+			
+			
 			double x_new = ray.getBlockPos().getX() + 0.5D;
 			double y_new = (ray.getBlockPos().getY() + 1);
 			double z_new = ray.getBlockPos().getZ() + 0.5D;
@@ -37,10 +59,64 @@ public class Teleport extends Module {
 						this.mc.thePlayer.posZ + (z_new - this.mc.thePlayer.func_174811_aO().getFrontOffsetZ()
 								- this.mc.thePlayer.posZ) * d / distance);
 			setPos(x_new, y_new, z_new);
+			mc.thePlayer.sendQueue.noEventPacket(new C13PacketPlayerAbilities(pc));
 			this.mc.renderGlobal.loadRenderers();
 		}
 	}
 
+	@Handler
+	public void EventRender(EventRenderer3D e) {
+		MovingObjectPosition ray = rayTrace(500.0D);
+		if (ray == null) return;
+		double x = ray.getBlockPos().getX() + 0.5D;
+		double y = (ray.getBlockPos().getY() + 1);
+		double z = ray.getBlockPos().getZ() + 0.5D;
+		drawBlock(new Vec3(x, y, z));
+	}
+	
+    private void drawBlock(Vec3 vec) {
+        double x = vec.getX() - RenderManager.renderPosX;
+        double y = vec.getY() - RenderManager.renderPosY;
+        double z = vec.getZ() - RenderManager.renderPosZ;
+        double width = 0.3;
+        double height = mc.thePlayer.getEyeHeight();
+        RenderUtils.pre3D();
+        GL11.glLoadIdentity();
+        mc.entityRenderer.setupCameraTransform(mc.timer.renderPartialTicks, 2);
+        int[] colors = { ColorUtilsArray.getColor(Color.black), ColorUtilsArray.getColor(Color.white) };
+        for (int i = 0; i < 2; i++) {
+            RenderUtils.glColor(colors[i]);
+            GL11.glLineWidth(3 - i * 2);
+            GL11.glBegin(GL11.GL_LINE_STRIP);
+            GL11.glVertex3d(x - width, y, z - width);
+            GL11.glVertex3d(x - width, y, z - width);
+            GL11.glVertex3d(x - width, y + height, z - width);
+            GL11.glVertex3d(x + width, y + height, z - width);
+            GL11.glVertex3d(x + width, y, z - width);
+            GL11.glVertex3d(x - width, y, z - width);
+            GL11.glVertex3d(x - width, y, z + width);
+            GL11.glEnd();
+            GL11.glBegin(GL11.GL_LINE_STRIP);
+            GL11.glVertex3d(x + width, y, z + width);
+            GL11.glVertex3d(x + width, y + height, z + width);
+            GL11.glVertex3d(x - width, y + height, z + width);
+            GL11.glVertex3d(x - width, y, z + width);
+            GL11.glVertex3d(x + width, y, z + width);
+            GL11.glVertex3d(x + width, y, z - width);
+            GL11.glEnd();
+            GL11.glBegin(GL11.GL_LINE_STRIP);
+            GL11.glVertex3d(x + width, y + height, z + width);
+            GL11.glVertex3d(x + width, y + height, z - width);
+            GL11.glEnd();
+            GL11.glBegin(GL11.GL_LINE_STRIP);
+            GL11.glVertex3d(x - width, y + height, z + width);
+            GL11.glVertex3d(x - width, y + height, z - width);
+            GL11.glEnd();
+        }
+
+        RenderUtils.post3D();
+    }
+    
 	public MovingObjectPosition rayTrace(double blockReachDistance) {
 		Vec3 vec3 = this.mc.thePlayer.func_174824_e(1.0F);
 		Vec3 vec4 = this.mc.thePlayer.getLookVec();
@@ -52,4 +128,12 @@ public class Teleport extends Module {
 		this.mc.thePlayer.sendQueue.addToSendQueue((Packet) new C03PacketPlayer.C04PacketPlayerPosition(x, y, z, true));
 		this.mc.thePlayer.setPosition(x, y, z);
 	}
+	
+    public static Block getBlock(final int x, final int y, final int z) {
+        return mc.theWorld.getBlockState(new BlockPos(x, y, z)).getBlock();
+    }
+
+    public static Block getBlock(final BlockPos pos) {
+        return mc.theWorld.getBlockState(pos).getBlock();
+    }
 }
